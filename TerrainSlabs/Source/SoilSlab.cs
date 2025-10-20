560585c571f801245f4237a858dd9503d46665a4
BeloMaximka <belomaxim@gmail.com>
2025-10-21 02:21:55 +0300

    Made soil slab look better

--- TerrainSlabs/Source/SoilSlab.cs
+++ TerrainSlabs/Source/SoilSlab.cs
@@ -1,8 +1,20 @@
-﻿using Vintagestory.GameContent;
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
 
 namespace TerrainSlabs.Source;
 
 public class SoilSlab : BlockSoil
 {
+    public override bool CanAttachBlockAt(IBlockAccessor blockAccessor, Block block, BlockPos pos, BlockFacing blockFace, Cuboidi attachmentArea = null)
+    {
+        if (block.Code.Path.StartsWithFast("snowlayer"))
+        {
+            // TODO: Adde snowlayeroffset and replace falling layer
+            return true;
+        }
 
+        return base.CanAttachBlockAt(blockAccessor, block, pos, blockFace, attachmentArea);
+    }
 }
