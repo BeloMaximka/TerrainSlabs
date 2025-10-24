ae11a0d7ebb50c0a2b523b3d120168b8a340a2e3
BeloMaximka <belomaxim@gmail.com>
2025-10-24 21:22:51 +0300

    Fixed selection boxes for offsets

---
+++ TerrainSlabs/Source/HarmonyPatches/AABBIntersectionTestPatch.cs
@@ -0,0 +1,104 @@
+﻿using HarmonyLib;
+using TerrainSlabs.Source.Utils;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+#pragma warning disable S101 // Types should be named in PascalCase
+
+namespace TerrainSlabs.Source.HarmonyPatches;
+
+[HarmonyPatch(typeof(AABBIntersectionTest), nameof(AABBIntersectionTest.RayIntersectsBlockSelectionBox))]
+public static class AABBIntersectionTestPatch
+{
+    [HarmonyPrefix]
+    public static bool OffsetSelectionBoxes(
+        AABBIntersectionTest __instance,
+        BlockPos pos,
+        BlockFilter filter,
+        bool testCollide,
+        ref bool __result,
+        ref Cuboidd ___tmpCuboidd,
+        ref BlockFacing ___hitOnBlockFaceTmp,
+        ref Vec3d ___hitPositionTmp,
+        ref Block ___blockIntersected
+    )
+    {
+        Block blockBelow = __instance.bsTester.blockAccessor.GetBlock(pos.Down());
+        if (!SlabGroupHelper.IsSlab(blockBelow.BlockId))
+        {
+            pos.Up();
+            return true;
+        }
+        pos.Up();
+
+        Block block = __instance.bsTester.blockAccessor.GetBlock(pos, 2);
+        Cuboidf[] cuboidfArray;
+
+        if (block.SideSolid.Any)
+        {
+            cuboidfArray = testCollide
+                ? block.GetCollisionBoxes(__instance.bsTester.blockAccessor, pos)
+                : block.GetSelectionBoxes(__instance.bsTester.blockAccessor, pos);
+        }
+        else
+        {
+            block = __instance.bsTester.GetBlock(pos);
+            cuboidfArray = testCollide
+                ? block.GetCollisionBoxes(__instance.bsTester.blockAccessor, pos)
+                : __instance.bsTester.GetBlockIntersectionBoxes(pos);
+        }
+
+        if (cuboidfArray == null || (filter != null && !filter(pos, block)))
+        {
+            __result = false;
+            return false;
+        }
+
+        bool found = false;
+        bool foundIsDecor = false;
+
+        for (int index = 0; index < cuboidfArray.Length; ++index)
+        {
+            ___tmpCuboidd.Set(cuboidfArray[index]).Translate(pos.X, pos.InternalY - 0.5f, pos.Z);
+
+            if (__instance.RayIntersectsWithCuboid(___tmpCuboidd, ref ___hitOnBlockFaceTmp, ref ___hitPositionTmp))
+            {
+                bool isDecor = cuboidfArray[index].GetType().Name == "DecorSelectionBox"; // internal type workaround
+
+                if (
+                    !found
+                    || !(!foundIsDecor | isDecor)
+                    || __instance.hitPosition.SquareDistanceTo(__instance.ray.origin)
+                        > ___hitPositionTmp.SquareDistanceTo(__instance.ray.origin)
+                )
+                {
+                    __instance.hitOnSelectionBox = index;
+                    found = true;
+                    foundIsDecor = isDecor;
+                    __instance.hitOnBlockFace = ___hitOnBlockFaceTmp;
+                    __instance.hitPosition.Set(___hitPositionTmp);
+                }
+            }
+        }
+
+        // Handle DecorSelectionBox.PosAdjust via reflection
+        if (found && cuboidfArray[__instance.hitOnSelectionBox].GetType().Name == "DecorSelectionBox")
+        {
+            var decorType = cuboidfArray[__instance.hitOnSelectionBox].GetType();
+            var posAdjustProp = decorType.GetProperty("PosAdjust");
+            Vec3i? posAdjust = posAdjustProp?.GetValue(cuboidfArray[__instance.hitOnSelectionBox]) as Vec3i;
+
+            if (posAdjust != null)
+            {
+                pos.Add(posAdjust);
+                block = __instance.bsTester.GetBlock(pos);
+            }
+        }
+
+        if (found)
+            ___blockIntersected = block;
+
+        __result = found;
+        return false; // skip original
+    }
+}
---
+++ TerrainSlabs/Source/HarmonyPatches/SystemSelectedBlockOutlinePatch.cs
@@ -0,0 +1,72 @@
+﻿using HarmonyLib;
+using TerrainSlabs.Source.Utils;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.Client.NoObf;
+
+namespace TerrainSlabs.Source.HarmonyPatches;
+
+[HarmonyPatch]
+public static class SystemSelectedBlockOutlinePatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(SystemSelectedBlockOutline), nameof(SystemSelectedBlockOutline.OnRenderFrame3DPost))]
+    public static bool OffsetSelectionBoxes(ClientMain ___game, WireframeCube ___cubeWireFrame)
+    {
+        if (!ClientSettings.SelectedBlockOutline)
+            return false;
+        float wireframethickness = ClientSettings.Wireframethickness;
+        if (!___game.ShouldRender2DOverlays || ___game.BlockSelection == null)
+            return false;
+        BlockPos pos = ___game.BlockSelection.Position;
+        if (___game.BlockSelection.DidOffset)
+            pos = pos.AddCopy(___game.BlockSelection.Face.Opposite);
+
+        // Our code
+        Block blockUnder = ___game.WorldMap.RelaxedBlockAccess.GetBlock(pos.Down());
+        pos.Up();
+        if (!SlabGroupHelper.IsSlab(blockUnder.BlockId))
+        {
+            return true;
+        }
+        Block solidBlock = ___game.WorldMap.RelaxedBlockAccess.GetBlock(pos);
+        if (solidBlock.SideSolid.OnSide(BlockFacing.DOWN))
+        {
+            return true;
+        }
+        // End
+        Block block = ___game.WorldMap.RelaxedBlockAccess.GetBlock(pos, 2);
+        Cuboidf[] cuboidfArray;
+        if (block.SideSolid.Any)
+        {
+            cuboidfArray = block.GetSelectionBoxes(___game.WorldMap.RelaxedBlockAccess, pos);
+        }
+        else
+        {
+            block = ___game.WorldMap.RelaxedBlockAccess.GetBlock(pos);
+            cuboidfArray = ___game.GetBlockIntersectionBoxes(pos);
+        }
+        if (cuboidfArray == null || cuboidfArray.Length == 0)
+            return false;
+        bool flag = block.DoParticalSelection((IWorldAccessor)___game, pos);
+        Vec4f selectionColor = block.GetSelectionColor((ICoreClientAPI)___game.api, pos);
+        double num1 = (double)pos.X + ___game.Player.Entity.CameraPosOffset.X;
+        double num2 = (double)pos.InternalY + ___game.Player.Entity.CameraPosOffset.Y - 0.5f; // our change
+        double num3 = (double)pos.Z + ___game.Player.Entity.CameraPosOffset.Z;
+        for (int index = 0; index < cuboidfArray.Length; ++index)
+        {
+            if (flag)
+                index = ___game.BlockSelection.SelectionBoxIndex;
+            if (cuboidfArray.Length <= index)
+                break;
+            Cuboidf cuboidf = cuboidfArray[index];
+            // I removed check for DecorSelectionBox, let's see what happens
+            ___cubeWireFrame.Render((ICoreClientAPI)___game.api, num1 + (double)cuboidf.X1, num2 + (double)cuboidf.Y1, num3 + (double)cuboidf.Z1, cuboidf.XSize, cuboidf.YSize, cuboidf.ZSize, 1.6f * wireframethickness, selectionColor);
+            if (flag)
+                break;
+        }
+
+        return false;
+    }
+}
