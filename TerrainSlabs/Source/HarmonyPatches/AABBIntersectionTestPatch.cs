906539f5f03d2d2d93b646eab6dc77f28e493352
BeloMaximka <belomaxim@gmail.com>
2025-10-28 05:30:48 +0200

    Rewrote AABBIntersectionTestPatch using transpilers

--- TerrainSlabs/Source/HarmonyPatches/AABBIntersectionTestPatch.cs
+++ TerrainSlabs/Source/HarmonyPatches/AABBIntersectionTestPatch.cs
@@ -1,7 +1,11 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using TerrainSlabs.Source.Utils;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
+using Vintagestory.API.Util;
 
 #pragma warning disable S101 // Types should be named in PascalCase
 
@@ -10,100 +14,41 @@ namespace TerrainSlabs.Source.HarmonyPatches;
 [HarmonyPatch(typeof(AABBIntersectionTest), nameof(AABBIntersectionTest.RayIntersectsBlockSelectionBox))]
 public static class AABBIntersectionTestPatch
 {
-    [HarmonyPrefix]
-    public static bool OffsetSelectionBoxes(
-        AABBIntersectionTest __instance,
-        BlockPos pos,
-        BlockFilter filter,
-        bool testCollide,
-        ref bool __result,
-        ref Cuboidd ___tmpCuboidd,
-        ref BlockFacing ___hitOnBlockFaceTmp,
-        ref Vec3d ___hitPositionTmp,
-        ref Block ___blockIntersected
-    )
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
-        Block blockBelow = __instance.bsTester.blockAccessor.GetBlock(pos.Down());
-        if (!SlabGroupHelper.IsSlab(blockBelow.BlockId))
+        MethodInfo method = AccessTools.Method(typeof(AABBIntersectionTestPatch), nameof(GetOffset));
+        FieldInfo bsTesterField = AccessTools.Field(typeof(AABBIntersectionTest), "bsTester");
+        MethodInfo accessorGetter = AccessTools.PropertyGetter(typeof(IWorldIntersectionSupplier), "blockAccessor");
+        MethodInfo internalYGetter = AccessTools.PropertyGetter(typeof(BlockPos), nameof(BlockPos.InternalY));
+
+        return new CodeMatcher(instructions, generator)
+            .Start()
+            .DeclareLocal(typeof(double), out LocalBuilder localVariable)
+            .InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldfld, bsTesterField),
+                new CodeInstruction(OpCodes.Callvirt, accessorGetter),
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Call, method),
+                new CodeInstruction(OpCodes.Stloc, localVariable.LocalIndex)
+            )
+            .MatchEndForward(
+                new CodeMatch(OpCodes.Ldarg_1),
+                new CodeMatch(OpCodes.Callvirt, internalYGetter),
+                new CodeMatch(OpCodes.Conv_R8)
+            )
+            .Advance(1)
+            .ThrowIfNotMatchForward("Could not find pos.InternalY")
+            .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc, localVariable.LocalIndex), new CodeInstruction(OpCodes.Add))
+            .InstructionEnumeration();
+    }
+
+    private static double GetOffset(IBlockAccessor accessor, BlockPos pos)
+    {
+        if (SlabGroupHelper.ShouldOffset(accessor.GetBlockId(pos)) && SlabGroupHelper.IsSlab(accessor.GetBlockBelow(pos).BlockId))
         {
-            pos.Up();
-            return true;
+            return -0.5d;
         }
-        pos.Up();
-        Block solidBlock = __instance.bsTester.blockAccessor.GetBlock(pos);
-        if (!SlabGroupHelper.ShouldOffset(solidBlock))
-        {
-            return true;
-        }
-
-        Block block = __instance.bsTester.blockAccessor.GetBlock(pos, 2);
-        Cuboidf[] cuboidfArray;
-
-        if (block.SideSolid.Any)
-        {
-            cuboidfArray = testCollide
-                ? block.GetCollisionBoxes(__instance.bsTester.blockAccessor, pos)
-                : block.GetSelectionBoxes(__instance.bsTester.blockAccessor, pos);
-        }
-        else
-        {
-            block = __instance.bsTester.GetBlock(pos);
-            cuboidfArray = testCollide
-                ? block.GetCollisionBoxes(__instance.bsTester.blockAccessor, pos)
-                : __instance.bsTester.GetBlockIntersectionBoxes(pos);
-        }
-
-        if (cuboidfArray == null || (filter != null && !filter(pos, block)))
-        {
-            __result = false;
-            return false;
-        }
-
-        bool found = false;
-        bool foundIsDecor = false;
-
-        for (int index = 0; index < cuboidfArray.Length; ++index)
-        {
-            ___tmpCuboidd.Set(cuboidfArray[index]).Translate(pos.X, pos.InternalY - 0.5f, pos.Z);
-
-            if (__instance.RayIntersectsWithCuboid(___tmpCuboidd, ref ___hitOnBlockFaceTmp, ref ___hitPositionTmp))
-            {
-                bool isDecor = cuboidfArray[index].GetType().Name == "DecorSelectionBox"; // internal type workaround
-
-                if (
-                    !found
-                    || !(!foundIsDecor | isDecor)
-                    || __instance.hitPosition.SquareDistanceTo(__instance.ray.origin)
-                        > ___hitPositionTmp.SquareDistanceTo(__instance.ray.origin)
-                )
-                {
-                    __instance.hitOnSelectionBox = index;
-                    found = true;
-                    foundIsDecor = isDecor;
-                    __instance.hitOnBlockFace = ___hitOnBlockFaceTmp;
-                    __instance.hitPosition.Set(___hitPositionTmp);
-                }
-            }
-        }
-
-        // Handle DecorSelectionBox.PosAdjust via reflection
-        if (found && cuboidfArray[__instance.hitOnSelectionBox].GetType().Name == "DecorSelectionBox")
-        {
-            var decorType = cuboidfArray[__instance.hitOnSelectionBox].GetType();
-            var posAdjustProp = decorType.GetProperty("PosAdjust");
-            Vec3i? posAdjust = posAdjustProp?.GetValue(cuboidfArray[__instance.hitOnSelectionBox]) as Vec3i;
-
-            if (posAdjust != null)
-            {
-                pos.Add(posAdjust);
-                block = __instance.bsTester.GetBlock(pos);
-            }
-        }
-
-        if (found)
-            ___blockIntersected = block;
-
-        __result = found;
-        return false; // skip original
+        return 0d;
     }
 }
--- TerrainSlabs/Source/HarmonyPatches/BlockDamagePatch.cs
+++ TerrainSlabs/Source/HarmonyPatches/BlockDamagePatch.cs
@@ -16,37 +16,19 @@ public static class BlockDamagePatch
 {
     public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
-        var codes = new List<CodeInstruction>(instructions);
-
         MethodInfo method = AccessTools.Method(typeof(BlockDamagePatch), nameof(GetOffset));
-        if (method == null)
-            return codes;
 
         FieldInfo decalOriginField = AccessTools.Field(typeof(SystemRenderDecals), "decalOrigin");
-        if (decalOriginField == null)
-            return codes;
         var blockDecalType = Type.GetType("Vintagestory.Client.NoObf.BlockDecal, VintagestoryLib");
-        if (blockDecalType == null)
-            return codes;
         FieldInfo decalPosField = AccessTools.Field(blockDecalType, "pos");
-        if (decalPosField == null)
-            return codes;
 
         FieldInfo gameField = AccessTools.Field(typeof(SystemRenderDecals), "game");
-        if (gameField == null)
-            return codes;
         FieldInfo worldMapField = AccessTools.Field(gameField.FieldType, "WorldMap");
-        if (worldMapField == null)
-            return codes;
         FieldInfo relaxedField = AccessTools.Field(worldMapField.FieldType, "RelaxedBlockAccess");
-        if (relaxedField == null)
-            return codes;
 
         FieldInfo yField = AccessTools.Field(decalOriginField.FieldType, "Y") ?? AccessTools.Field(decalOriginField.FieldType, "y");
-        if (yField == null)
-            return codes;
 
-        return new CodeMatcher(codes, generator)
+        return new CodeMatcher(instructions, generator)
             .Start()
             .DeclareLocal(typeof(float), out LocalBuilder localVariable)
             .InsertAndAdvance(
@@ -59,20 +41,21 @@ public static class BlockDamagePatch
                 new CodeInstruction(OpCodes.Call, method),
                 new CodeInstruction(OpCodes.Stloc, localVariable.LocalIndex)
             )
-            .MatchStartForward(
+            .MatchEndForward(
                 new CodeMatch(OpCodes.Ldarg_0),
                 new CodeMatch(OpCodes.Ldfld, decalOriginField),
                 new CodeMatch(OpCodes.Ldfld, yField),
                 new CodeMatch(OpCodes.Sub)
             )
+            .Advance(1)
             .ThrowIfNotMatchForward("Could not find (float)decal.pos.Y - (float)this.decalOrigin.Y")
             .InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc, localVariable.LocalIndex), new CodeInstruction(OpCodes.Add))
             .InstructionEnumeration();
     }
 
-    public static double GetOffset(IBlockAccessor accessor, BlockPos pos)
+    private static double GetOffset(IBlockAccessor accessor, BlockPos pos)
     {
-        if (SlabGroupHelper.IsSlab(accessor.GetBlockBelow(pos).BlockId))
+        if (SlabGroupHelper.ShouldOffset(accessor.GetBlockId(pos)) && SlabGroupHelper.IsSlab(accessor.GetBlockBelow(pos).BlockId))
         {
             return -0.5f;
         }
